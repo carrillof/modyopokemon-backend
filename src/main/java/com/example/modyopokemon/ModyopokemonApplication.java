9e83c82031f7a706cd18deb0a4bd803c1f0964fd
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2025-03-27 12:31:24 -0600

    Initial commit

---
+++ src/main/java/com/example/modyopokemon/ModyopokemonApplication.java
@@ -0,0 +1,13 @@
+package com.example.modyopokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModyopokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModyopokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/modyopokemon/ModyopokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modyopokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModyopokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
